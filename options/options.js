2da343a70bcc57ba992bef83f3e51f82acc36ef8
Crowdbotics <team@crowdbotics.com>
2024-01-24 17:12:30 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "salon-scheduler-pro-45514",
+  url: "https://salon-scheduler-pro-45514.botics.co",
+  api: "https://salon-scheduler-pro-45514.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
